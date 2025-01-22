b827668b1292304e4e5ceb3a58c5e42539f2be58
Dorten8 <jsej@itu.dk>
2025-01-22 17:23:54 +0100

    manually deleted extra folder garbage_v2 in garbage_v2 folder...no idea why, project cleaned and rebuilt, all functionality checked

--- garbage_v3/app/src/main/java/com/example/garbage_v3/MainActivity.kt
+++ garbage_v3/app/src/main/java/com/example/garbage_v3/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.a2025_garbage_mad
+package com.example.garbage_v3
 
 import android.content.Intent
 import android.os.Bundle
@@ -10,10 +10,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.garbage_v3.AddNewItemActivity
-import com.example.garbage_v3.ItemsToSortDB
-import com.example.garbage_v3.ListActivity
-import com.example.garbage_v3.R
 
 class MainActivity : AppCompatActivity() {
 
